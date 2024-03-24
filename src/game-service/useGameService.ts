bafec65e840713124cce3d8a409ecf0ebf0cb3a3
Ishmael Smyrnow <ismyrnow@gmail.com>
2024-03-24 01:29:47 -0400

    sync state after starting or joining a game

--- src/game-service/useGameService.ts
+++ src/game-service/useGameService.ts
@@ -5,19 +5,27 @@ export default function useGameService(existingGameId: string | null = null) {
   const [gameId, setGameId] = useState<string | null>(existingGameId);
   const [board, setBoard] = useState(gameService.getBoard());
   const [nextPiece, setNextPiece] = useState(gameService.getNextPiece());
+  const syncState = () => {
+    setBoard(gameService.getBoard());
+    setNextPiece(gameService.getNextPiece());
+  };
 
   useEffect(() => {
     async function initializeGame() {
       if (existingGameId) {
+        console.log("Joining game", existingGameId);
         await gameService.joinGame(existingGameId);
       } else {
+        console.log("Creating game");
         await gameService.createGame();
         setGameId(gameService.getGameId());
       }
+
       gameService.subscribe(() => {
-        setBoard(gameService.getBoard());
-        setNextPiece(gameService.getNextPiece());
+        syncState();
       });
+
+      syncState();
     }
 
     initializeGame();
