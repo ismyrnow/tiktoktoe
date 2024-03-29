a20b14dfee948829261cf5ba843864c04e838f55
Ishmael Smyrnow <ismyrnow@gmail.com>
2024-03-28 22:39:33 -0400

    fix board and piece color in light theme
    fixes #3

--- src/Cross.tsx
+++ src/Cross.tsx
@@ -1,15 +1,10 @@
 export default function Cross() {
   return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      stroke="#ffffff"
-    >
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g>
         <path
           d="M19 5L5 19M5 5L19 19"
-          stroke="#ffffff"
+          stroke="currentColor"
           strokeWidth="1.5"
           strokeLinecap="round"
           strokeLinejoin="round"
